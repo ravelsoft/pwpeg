75a08d8ed4e207c4829889afda6106c5b91f6a97
Christophe Eymard <christophe.eymard@ravelsoft.com>
2011-12-03 17:41:29 +0100

    Initial import.

---
+++ src/pwpeg.py
@@ -0,0 +1,420 @@
+"""
+    @author Christophe Eymard <christophe@ravelsoft.com>
+"""
+
+from functools import wraps
+from re import _pattern_type
+import inspect
+
+
+class SyntaxError(Exception):
+    """ The way the text is parsed is by trial and error.
+
+        When SyntaxError is caught by the parser, it goes up the caller
+        chain to get to the first Choice() it finds to keep looking
+        for a match.
+    """
+
+class IgnoreResult(object):
+    """ This class is used by the parsing rules to determine wether to add the
+        result of some parsing to the general result.
+
+        Typically, the And and Not rules return this as well as and advancement
+        of 0 when they match.
+    """
+
+    pass
+
+class Rule(object):
+    """ A Rule.
+    """
+
+    class ArmedRule(object):
+        """
+        """
+
+        def __init__(self, rule, *args, **kwargs):
+            self.args = args
+            self.kwargs = kwargs
+            self.rule = rule
+
+        def __call__(self, text, skip=None):
+            return self.rule.parse(text, rules=self.rule.rulefn(*self.args, **self.kwargs), skip=skip)
+
+
+    @staticmethod
+    def from_function(func, direct_processing=None):
+        """ Create a Rule which rule function is func.
+        """
+
+        if direct_processing:
+            return Rule(func, direct_processing)
+        else:
+            return Rule(func)
+
+
+    def __call__(self, *args, **kwargs):
+        """  
+        """
+
+        return Rule.ArmedRule(self, *args, **kwargs)
+
+
+    def __init__(self, *args):
+        """
+        """
+
+        if len(args) == 0:
+            raise Exception("Can not have empty rules")
+
+        if callable(args[0]) and not isinstance(args[0], Rule) and not isinstance(args[0], Rule.ArmedRule):
+            # Passing a rule function
+            self.rulefn = args[0]
+            self.name = args[0].__name__
+        else:
+            self.rulefn = lambda: args
+            self.name = self.__class__.__name__
+        
+
+
+    def _parse_terminal(self, text, terminal):
+        """ Try to match text.
+        """
+
+        if text.startswith(terminal):
+            return (len(terminal), terminal)
+        return False
+
+
+    def _parse_regexp(self, text, reg):
+        """ Try to match a regexp and returns its match.
+        """
+
+        m = reg.match(text)
+        if m and len(m.group(0)) > 0:
+            return len(m.group(0)), m.group(0)
+        return False
+
+
+    def parse(self, text, rules=None, skip=None):
+        """ Execute our own rules.
+        """
+
+        advanced = 0
+        results = []
+
+        if not isinstance(rules, tuple):
+            rules = (rules,)
+        
+        for i, r in enumerate(rules):
+            subrule_result = None
+
+            if skip:
+                try:
+                    adv, res = skip(text[advanced:])
+                    advanced += adv
+                except SyntaxError, e:
+                    # Nothing to skip.
+                    pass
+
+            # We are checking a simple text terminal
+            if isinstance(r, unicode) or isinstance(r, str):
+                subrule_result = self._parse_terminal(text[advanced:], r)
+
+            # We have to handle a regexp.
+            elif isinstance(r, _pattern_type):
+                subrule_result = self._parse_regexp(text[advanced:], r)
+
+            # We have an armed rule, which technically means that the rule function of
+            # the rule has already been evaluated.
+            elif isinstance(r, Rule.ArmedRule):
+                subrule_result = r(text[advanced:], skip=skip)
+
+            # We have a rule that hasn't be armed. We assume that it's rule function doesn't
+            # take arguments.
+            elif isinstance(r, Rule):
+                r = r()
+                subrule_result = r(text[advanced:], skip=skip)
+
+            # Otherwise, this is just a construct that does some checking of its own.
+            else:
+                # The construct is called with the results we have so far.
+                # It will raise SyntaxError if it returns false.
+                res = r(*results)
+
+                if res is False:
+                    raise SyntaxError("Check returned false")
+
+                # If this function was the last rule, it means it was actually a processor
+                # function given as a lambda, so we return its results.
+                if i == len(rules) - 1:
+                    return advanced, res
+
+                continue
+
+            if subrule_result is False:
+                # FIXME: we should be more explicit here !
+                # Also, we should keep in Choices a list of all the tried combination
+                # that failed, as well as the position where the failing was
+                # found.
+
+                # The subrule_result returning false is only when matching a terminal.
+                raise SyntaxError(repr(r))
+
+            # If everything went according to plan, the subrule_result is an tuple
+            # with the number of consumed characters and the result of the processing
+            # of the rule.
+            sub_adv, sub_processed_result = subrule_result
+
+            advanced += sub_adv
+
+            if sub_processed_result is not IgnoreResult:
+                results.append(sub_processed_result)
+
+        if len(results) == 1:
+            return advanced, results[0]
+
+        return advanced, results
+
+
+    def set_processer(self, fn):
+        """ Add a processor function to the rules.
+        """
+
+        rulesfn = self.rulesfn
+        self.rulesfn = lambda *args, **kwargs: rulesfn(*args, **kwargs) + (fn,)
+        return fn
+
+    def __repr__(self):
+        return "<%s>" % self.name
+
+
+
+class Repetition(Rule):
+    """ A set of rules that can be repeated.
+    """
+
+
+    def __init__(self, _from, _to, *args):
+        self._from = _from
+        self._to = _to
+        super(Repetition, self).__init__(*args)
+
+
+    def parse(self, text, rules, skip=None):
+        times = 0
+        _from, _to = self._from, self._to
+
+        advance = 0
+
+        result = []
+
+        while advance < len(text) and (_to == -1 or times < _to):
+            try:
+                # Get the results.
+                adv, res = super(Repetition, self).parse(text[advance:], rules, skip)
+            except SyntaxError, e:
+                break
+
+            # Parsing was successful, so we add it to the result.
+            advance += adv
+
+            if res is not IgnoreResult:
+                result.append(res)
+
+            # We repeated one more time !
+            times += 1
+
+        if _from != -1 and times < _from:
+            raise SyntaxError("Rule needs to be repeated at least %d times" % _from)
+
+        return advance, result
+
+
+
+class OneOrMore(Repetition):
+
+    def __init__(self, *args, **kwargs):
+        super(OneOrMore, self).__init__(1, -1, *args)
+
+
+
+class ZeroOrMore(Repetition):
+
+    def __init__(self, *args, **kwargs):
+        super(ZeroOrMore, self).__init__(0, -1, *args)
+
+
+
+class Optional(Repetition):
+
+    def __init__(self, *args, **kwargs):
+        super(Optional, self).__init__(0, 1, *args)
+
+
+
+class Not(Rule):
+    """ Look ahead in the input. If no syntax error is received, then
+        raise a SyntaxError.
+
+        This rule is used to check that a rule does not apply on the input
+        following the current position.
+
+        It does *not* advance the parser position.
+    """
+
+    def parse(self, text, rules, skip):
+        try:
+            super(Not, self).parse(text, rules, skip)
+            raise SyntaxError("Matched input that should not have been matched")
+        except SyntaxError, e:
+            # Couldn't match the next rule, which is what we want, so
+            # we return a result that won't advance the parser.
+            return 0, IgnoreResult
+                
+
+
+class And(Rule):
+    """ Look ahead in the input. If its rules can be applied, then continue
+        parsing from the same place we were before executing this rule.
+
+        This rule is used to check that the following input contains a rule we
+        want.
+
+        It does *not* advance the parser position.
+    """
+
+    
+    def parse(self, text, rules, skip):
+        # Try to parse our rules
+        super(And, self).parse(text, rules, skip)
+
+        # If there was no error, we don't advance, which is what we want.
+        # A syntax error is raised otherwise in our super() parse.
+        return 0, IgnoreResult
+
+
+
+class Either(Rule):
+    """ Try parsing with several rules and return the result of the first
+        one that works.
+    """
+
+    def __init__(self, *args):
+        self.rules = []
+
+        for a in args:
+            if isinstance(a, tuple):
+                self.rules.append(Rule(*a))
+            else:
+                self.rules.append(Rule(a))
+
+        self.rulefn = lambda: None
+        self.name = "Choice<>"
+
+
+    def parse(self, text, rules, skip):
+
+        for rule in self.rules:
+            try:
+                r = rule()
+                return r(text, skip)
+            except SyntaxError, e:
+                # FIXME should store the error
+
+                # We continue since the SyntaxError just means that we didn't match and
+                # must try the next choice.
+                continue
+
+        raise SyntaxError("None of the provided choices matched")
+
+
+
+class R(Rule):
+    """ A proxy Rule to call rules not yet defined in the scope.
+
+        What it does is by using the `inspect` module, it stores a reference
+        to the module where it is instanciated as well as the name of the rule
+        in said module. 
+        
+        When the rule is to be used for parsing, it fetches it from the module
+        and executes it.
+
+        This means that this Rule can be used to refer to rules that will be
+        defined later in the code, or even the current rule if we need
+        recursion.
+    """
+    
+    def __init__(self, rulename, *args, **kwargs):
+
+        c = inspect.currentframe()
+        self.gl = c.f_back.f_globals
+        self.name = rulename
+
+
+    def parse(self, *args, **kwargs):
+        raise Exception("This method should never be called directly.")
+
+
+    def __call__(self, *args, **kwargs):
+        """ Return the result of calling the real rule after fetching
+            it from its global object.
+        """
+
+        return self.gl[self.name](*args, **kwargs)
+
+
+
+class Parser(object):
+    """ A parser that parses a text input.
+
+        It is given a top-level rule with which it will start the parsing,
+        as well as a skip rule which will be checked agains before executing
+        any rule, useful to remove white spaces and comments.
+    """
+
+    def __init__(self, toprule, skiprule=None):
+
+        if not isinstance(toprule, Rule):
+            toprule = Rule(toprule)
+
+        if not isinstance(toprule, Rule.ArmedRule):
+            self.toprule_parse = toprule()
+        else:
+            self.toprule_parse = toprule
+
+        if skiprule:
+            if not isinstance(skiprule, Rule):
+                skiprule = Rule(skiprule)
+
+            self.skiprule_parse = skiprule()
+        else:
+            self.skiprule_parse = None
+
+
+    def parse(self, text):
+        """ Parse the given input and return the result of the parsing.
+
+            An Exception will be raised if the parsing does not use the
+            integrality of the text.
+        """
+
+        result = self.toprule_parse(text, skip=self.skiprule_parse)
+
+        if result[0] != len(text):
+            raise Exception("Finished parsing, but all the input was not consumed by the parser. Leftovers: %s" % text[result[0]:])
+
+        # Everything went fine, sending the results.
+        return result[1]
+
+
+    def partialParse(self, text):
+        """ Parse the given input and return only the result of the parsing,
+            which is a tuple containing the (number of consumed characters, result of parsing).
+
+            It can be used on some inputs on which we know the grammar can't or won't consume
+            the totality of the input.
+        """
+
+        return self.toprule_parse(text, skip=self.skiprule_parse)
+
