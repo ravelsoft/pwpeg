cca9233a0f59e81f3dbd11f3831adbd98fa7d939
Christophe Eymard <christophe@ravelsoft.com>
2012-01-19 18:12:29 +0100

    Version bump 0.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PwPEG',
-      version='0.2.3',
+      version='0.2.4',
       description='Python PEG Parsing Framework',
       author='Christophe Eymard',
       author_email='christophe.eymard@ravelsoft.com',
