ec3e803d63f0e2fc9853d29d9ed6b49d5dd29cbf
Christophe Eymard <christophe.eymard@gmail.com>
2013-05-12 11:38:20 +0200

    reworking of the RightAssociative and LeftAssociative helpers

--- python/pwpeg/helpers.py
+++ python/pwpeg/helpers.py
@@ -5,16 +5,31 @@ from functools import wraps
 from itertools import chain
 
 
-def _AllBut(but, escape):
-    """
+def _AllBut(but, escape=None, skip=None):
+    """ matches everything *but* the but parameter until it finds it,
+        unless it is escaped.
+
+        warning; this rule is very slow compared to its regexp version
+        and should only be used in token streams to get results more
+        interesting than a list of single characters.
+
+        Args:
+            but: the rule that we do not want to match.
+            escape: an escape rule to allow the but rule to be in the results.
+            skip: a custom skip; by default it is reset to None.
+        Results:
+            the corresponding Rule.
     """
 
-    return OneOrMore(
-        Either(
-            Rule(escape, but).set_action(lambda escape, but: but),
-            Rule(Not(but), Any())
-        )
-    ).set_skip(None)
+    if escape:
+        return OneOrMore(
+            Either(
+                Rule(escape, but).set_action(lambda escape, but: but),
+                Rule(Not(but), Any())
+            )
+        ).set_skip(skip)
+    else:
+        return OneOrMore(Not(but), Any()).set_skip(skip)
 
 AllBut = FunctionRule(_AllBut)
 AllBut.set_name("All But")
@@ -44,12 +59,13 @@ Balanced = FunctionRule(_Balanced)
 Balanced.set_name("Balanced")
 
 
-def _DelimitedBy(char, escape):
+def _DelimitedBy(delimiter, escape):
     """
     """
 
-    return char, AllBut.instanciate(char, escape), char
+    return delimiter, AllBut(delimiter, escape), delimiter
 DelimitedBy = FunctionRule(_DelimitedBy).set_skip(None)
+
 
 def _RepeatingSeparated(rule, separator, at_least, at_most):
     """
@@ -107,54 +123,110 @@ to_eol = Rule(re.compile("[ \t]*"))
 empty_lines = Rule(re.compile("([ \t]*\n)*", re.M))
 
 
-def la_tree_from_list_leftmost(leftmost, lst, action, idx=-1):
-    return action(
-        la_tree_from_list_leftmost(leftmost, lst, action, idx - 1),
-        lst[idx][0],
-        lst[idx][1]) if lst and idx >= -len(lst) else action(leftmost)
+def _associative(right=True):
 
-def ra_tree_from_list_rightmost(rightmost, lst, action, idx=0):
-    """
-    """
+    def _wrapped(production, operator, builder=None):
+        ''' Parse sub-expressions separated by a binary operator zero or
+            more times, and return the tree of their associativity in the
+            polish style notation (operator first.)
 
-    return action(lst[idx][0],
-                  lst[idx][1],
-                  ra_tree_from_list_rightmost(rightmost, lst, action, idx + 1)) if lst and idx < len(lst) else action(rightmost, None, None)
+            The builder is a callable taking three arguments; (operator, left, right)
+            that is called to build nodes if the desired result is more complicated
+            than tuples.
 
+            Ex: for an input of tokens "a+b-c" and the rule
+            RightAssociative(re.compile('\w+'), Either('+', '-')), the result shall be
+            ('+', 'a', ('-', 'b', 'c')) if no builder was provided.
 
-def _LeftAssociative(op_FunctionRule, sub_FunctionRule):
+            In the case there is no operators, then the result is the matched
+            element instead of a list.
+            Ex: for an input of token 'a', the result is 'a', not ['a']
 
-    def reverse(leftmost, lst, idx=-1):
-        """ Recursive function to make what we parsed as a left-associative succession of operators.
-        """
-        return la_tree_from_list_leftmost(leftmost, lst, action, idx)
+            Args:
+                production: the production rule.
+                operator: the operator rule.
+                builder: a callable(op, lhs, rhs)
+            Returns:
+                the resulting tree or a single matched elements if there was no operator.
+        '''
 
-    return Rule(sub_FunctionRule, ZeroOrMore(op_FunctionRule, sub_FunctionRule)).set_action(reverse)
-LeftAssociative = FunctionRule(_LeftAssociative)
+        # Default builder
+        builder = builder if builder else lambda op, lhs, rhs: (op, lhs, rhs)
 
-def _RightAssociative(op_FunctionRule, sub_FunctionRule):
+        def left_assoc(lst, idx=0):
+            idx = 1
+            l = len(lst)
+            tmpbuild = builder(lst[idx], lst[idx - 1], lst[idx + 1])
 
-    def _act(left, opt, idx=0):
-        """ Recursive function to make what we parsed as a left-associative succession of operators.
-        """
+            while idx < l - 2:
+                idx += 2
+                tmpbuild = builder(tmpbuild, lst[idx], lst[idx + 1])
 
-        return action(left) if not opt else action(left, opt[0], opt[1])
+            return tmpbuild
 
-    self_recurse = R("RightAssociative", op_FunctionRule, sub_FunctionRule, action)
+        def right_assoc(lst):
+            idx = len(lst) - 3
+            tmpbuild = builder(lst[idx + 1], lst[idx], lst[idx + 2])
 
-    return Rule(sub_FunctionRule, Optional(op_FunctionRule, self_recurse)).set_action(_act)
-RightAssociative = FunctionRule(_RightAssociative)
+            while idx > 0:
+                idx -= 2
+                tmpbuild = builder(lst[idx + 1], lst[idx], tmpbuild)
+
+            return tmpbuild
+
+        def action(first, lst):
+            if not lst:
+                return first
+
+            newlst = [first]
+            # First flatten the list, which is a series of [[op, operand], ...]
+            for item in lst:
+                newlst += item
+
+            return right_assoc(newlst) if right else left_assoc(newlst)
+
+        return Rule(production, ZeroOrMore(operator, production)).set_action(action)
+
+    return _wrapped
+
+RightAssociative = FunctionRule(_associative(right=True))
+LeftAssociative = FunctionRule(_associative(right=False))
 
 ####################################################################
 #       Regexp Helpers
 
 def delimitedby_regexp(char, escape):
+    '''
+        Create a regular expression describing a string delimited by a delimiter
+        with an escape character to match the delimiter inside it.
+
+        To match a correctly formed string; delimitedby_regexp('\'', '\\') will
+        create a regexp matching anything between two single quotes, where \\' will
+        escape it.
+
+        Args:
+            char: a regular expression representing a delimiter. Beware that if a
+                range is specified, then the other end will also be a range.
+                ['"] for instance will match strings like 'hello" or "hello'
+            escape: A regular expression for an escape sequence.
+        Returns:
+            a regular expression
+    '''
     return re.compile(u("{0}({1}{0}|(?!{0}).)*{0}").format(re.escape(char), re.escape(escape)), re.DOTALL)
 
+
 def allbut_regexp(patterns, escape):
+    '''
+        Match anything but *but* the provided patterns.
+
+        Args:
+            patterns: a list of regular expression patterns that we
+                don't want to match.
+            escape: an escape pattern allowing one of the forbidden
+                pattern to still be matched.
+        Returns:
+            a regular expression.
+    '''
     if not isinstance(patterns, list):
         patterns = [patterns]
     return re.compile("({1}({0})|(?!({0})).)+".format("|".join([re.escape(s) for s in patterns]), re.escape(escape)), re.DOTALL)
-
-def delimitedby_regexp_escapes(char, escapes):
-    return re.compile(u("{0}({1}|(?!{0}).)*{0}").format(re.escape(char), escapes))
